faba61369018b9825f8b6ad8a71869c83a27463f
AmitGit217 <amit217@yandex.com>
2026-02-07 20:59:18 +0100

    chore: ej 5.1

--- ej5.js
+++ ej5.js
@@ -40,3 +40,21 @@ const legendsStreamers = streamers3.filter(streamer => {
     }       
     return false;
 });
+
+
+
+
+const streamers4 = [
+	{name: 'Rubius', age: 32, gameMorePlayed: 'Minecraft'},
+	{name: 'Ibai', age: 25, gameMorePlayed: 'League of Legends'},
+	{name: 'Reven', age: 43, gameMorePlayed: 'League of Legends'},
+	{name: 'AuronPlay', age: 33, gameMorePlayed: 'Among Us'}
+];
+
+
+const input = document.querySelector('[data-function="toFilterStreamers"]');
+input.addEventListener('input', (event) => {
+    const value = event.target.value.toLowerCase();
+    const filteredStreamers = streamers4.filter(streamer => streamer.name.toLowerCase().includes(value));
+    console.log(filteredStreamers);
+});
