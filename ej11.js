230d4a03cc664020519dd59b5c08886eccd85f34
AmitGit217 <amit217@yandex.com>
2026-02-07 21:12:50 +0100

    chore: ej 11

---
+++ ej11.js
@@ -0,0 +1,55 @@
+
+
+const users = [
+  {
+    name: "Alberto",
+    favoritesSounds: {
+      waves: { format: "mp3", volume: 50 },
+      rain: { format: "ogg", volume: 60 },
+      firecamp: { format: "mp3", volume: 80 },
+    },
+  },
+  {
+    name: "Antonio",
+    favoritesSounds: {
+      waves: { format: "mp3", volume: 30 },
+      shower: { format: "ogg", volume: 55 },
+      train: { format: "mp3", volume: 60 },
+    },
+  },
+  {
+    name: "Pedro",
+    favoritesSounds: {
+      shower: { format: "mp3", volume: 50 },
+      train: { format: "ogg", volume: 60 },
+      firecamp: { format: "mp3", volume: 80 },
+    },
+  },
+  {
+    name: "Cristina",
+    favoritesSounds: {
+      waves: { format: "mp3", volume: 67 },
+      wind: { format: "ogg", volume: 35 },
+      firecamp: { format: "mp3", volume: 60 },
+    },
+  },
+];
+
+const soundCounts = {};
+
+for (const user of users) {
+  for (const sound in user.favoritesSounds) {
+    if (Object.prototype.hasOwnProperty.call(user.favoritesSounds, sound)) {
+      if (soundCounts[sound]) {
+        soundCounts[sound] += 1;
+      } else {
+        soundCounts[sound] = 1;
+      }
+    }
+  }
+}
+
+console.log('Conteo de sonidos favoritos:', soundCounts);
+
+
+
